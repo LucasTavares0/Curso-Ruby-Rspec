e9acdaed912801865dc1b12bb5ef2c27ad2aeb5c
Lucas Tavares <lucas.s_tavares@hotmail.com>
2020-12-15 10:06:04 -0300

    progresso aula 25 - 2

--- testes-unitarios/app/bank.rb
+++ testes-unitarios/app/bank.rb
@@ -1,4 +1,5 @@
-class ContaCorrente 
+class Conta 
+
     attr_accessor :saldo
     attr_accessor :mensagem
 
@@ -6,14 +7,31 @@ class ContaCorrente
         self.saldo = saldoInicial
     end
 
-    def sacar(valorSaque)
-        
+    def sacar(valorSaque, max, taxa)
         if(valorSaque > self.saldo)
             self.mensagem = 'Saldo insuficiente para saque'
         elsif(valorSaque > 700)
             self.mensagem = 'Saques limitados a R$700 por saque'       
         else
-            self.saldo -= valorSaque
+            self.saldo -= (valorSaque + taxa)
         end
-    end
+    end 
+
+end
+
+
+class ContaCorrente < Conta
+    
+    def sacar(valor, max = 700, taxa = 5)
+        super
+    end 
+
+end
+
+class ContaPoupanca < Conta
+
+    def sacar(valor, max = 500, taxa = 2)
+        super
+    end 
+
 end
